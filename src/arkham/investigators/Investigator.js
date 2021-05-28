858fff198e586bd1a155da6908f446781621b1a9
Mayjest <mayjest@gmail.com>
2021-05-28 22:35:43 +0100

    Prep Investigator to start loading from json

--- src/arkham/investigators/Investigator.js
+++ src/arkham/investigators/Investigator.js
@@ -8,19 +8,41 @@ import StatTracker from './StatTracker';
 
 class Investigator extends React.Component {
 
+    // profile = {
+    //     name: "Roland Banks",
+    //     class: "Guardian",
+    //     picture: rolandBanks,
+    //     maxDamage: 9,
+    //     maxSanity: 5,
+    //     stats: {
+    //         agility: 2,
+    //         willpower: 3,
+    //         intellect: 3,
+    //         combat: 4
+    //     }
+    // };
+
+    // status = {
+    //     physicalTrauma: 0,
+    //     mentalTrauma: 0
+    // }
+
+
     constructor(props){
         super(props);
-        let initDamage = 0;
-        let initSanity = 0;
-        if (props.physicalTrauma && props.physicalTrauma > 0){
-            initDamage = props.physicalTrauma;
-        }
-        if (props.mentalTrauma && props.mentalTrauma > 0) {
-            initSanity = props.mentalTrauma;
+        this.initDamage = 0;
+        this.initSanity = 0;
+        if (props.status) {
+            if (props.status.physicalTrauma && props.status.physicalTrauma > 0){
+                this.initDamage = props.status.physicalTrauma;
+            }
+            if (props.status.mentalTrauma && props.status.mentalTrauma > 0) {
+                this.initSanity = props.status.mentalTrauma;
+           }
         }
         this.state = {
-            currentDamage: initDamage,
-            currentSanity: initSanity
+            currentDamage: this.initDamage,
+            currentSanity: this.initSanity
         }
 
         this.reduceDamage = this.reduceDamage.bind(this);
@@ -30,32 +52,32 @@ class Investigator extends React.Component {
     }
 
     reduceDamage() {
-        if (this.state.currentDamage > 0){
-            this.setState(state => ({
+        if (this.state.currentDamage > this.initDamage){
+            this.setState(() => ({
                 currentDamage: this.state.currentDamage - 1
             }));
         }
     }
 
     increaseDamage() {
-        if (this.state.currentDamage < this.props.maxDamage) {
-            this.setState(state => ({
+        if (this.state.currentDamage < this.props.profile.maxDamage) {
+            this.setState(() => ({
                 currentDamage: this.state.currentDamage + 1
             }));
         }
     }
 
     reduceSanity() {
-        if (this.state.currentSanity > 0){
-            this.setState(state => ({
+        if (this.state.currentSanity > this.initSanity){
+            this.setState(() => ({
                 currentSanity: this.state.currentSanity - 1
             }));
         }
     }
 
     increaseSanity() {
-        if (this.state.currentSanity < this.props.maxSanity) {
-            this.setState(state => ({
+        if (this.state.currentSanity < this.props.profile.maxSanity) {
+            this.setState(() => ({
                 currentSanity: this.state.currentSanity + 1
             }));
         }
@@ -66,31 +88,31 @@ class Investigator extends React.Component {
             <div className="investigator">
                 <div className="investigator-wrapper">
                     <div className="investigator-profile">
-                        <span className="investigator-name">{this.props.name}</span>
-                        <span className="investigator-class">{this.props.class}</span>
+                        <span className="investigator-name">{this.props.profile.name}</span>
+                        <span className="investigator-class">{this.props.profile.class}</span>
                         <div className="investigator-skill">
-                            <span>{this.props.intellect}</span>
+                            <span>{this.props.profile.stats.intellect}</span>
                             <img src={intellect} alt="Intellect" width="28px" height="28px"></img>
                         </div>
                         <div className="investigator-skill">
-                            <span>{this.props.willpower}</span>
+                            <span>{this.props.profile.stats.willpower}</span>
                             <img src={willpower} alt="Willpower" width="28px" height="28px"></img>
                         </div>
                         <div className="investigator-skill">
-                            <span>{this.props.combat}</span>
+                            <span>{this.props.profile.stats.combat}</span>
                             <img src={combat} alt="Combat" width="28px" height="28px"></img>
                         </div>
                         <div className="investigator-skill">
-                            <span>{this.props.agility}</span>
+                            <span>{this.props.profile.stats.agility}</span>
                             <img src={agility} alt="Agility" width="28px" height="28px"></img>
                         </div>
                     </div>
                     <div className="investigator-picture">
-                        <img src={this.props.picture} alt={this.props.name}/>
+                        <img src={this.props.profile.picture} alt={this.props.profile.name}/>
                     </div>
                     <div className="investigator-stats">
-                        <StatTracker stat="Damage" color="Red" current={this.state.currentDamage} max={this.props.maxDamage} reduce={this.reduceDamage} increase={this.increaseDamage}/>
-                        <StatTracker stat="Sanity" color="Blue" current={this.state.currentSanity} max={this.props.maxSanity} reduce={this.reduceSanity} increase={this.increaseSanity}/>
+                        <StatTracker stat="Damage" color="Red" current={this.state.currentDamage} max={this.props.profile.maxDamage} reduce={this.reduceDamage} increase={this.increaseDamage}/>
+                        <StatTracker stat="Sanity" color="Blue" current={this.state.currentSanity} max={this.props.profile.maxSanity} reduce={this.reduceSanity} increase={this.increaseSanity}/>
                     </div>
                 </div>
             </div>
--- src/arkham/investigators/roland-banks/RolandBanks.js
+++ src/arkham/investigators/roland-banks/RolandBanks.js
@@ -4,9 +4,24 @@ import rolandBanks from './RolandBanksProfile-64.png';
 
 class RolandBanks extends React.Component {
 
+    investigatorData = {
+        name: "Roland Banks",
+        class: "Guardian",
+        picture: rolandBanks,
+        maxDamage: 9,
+        maxSanity: 5,
+        stats: {
+            agility: 2,
+            willpower: 3,
+            intellect: 3,
+            combat: 4 
+        }
+    };
+
     render() {
         return (
-            <Investigator name="Roland Banks" class="Guardian" picture={rolandBanks} maxDamage="9" maxSanity="5" agility="2" willpower="3" intellect="3" combat="4" />
+            <Investigator 
+                profile={this.investigatorData} status={{physicalTrauma: 3, mentalTrauma: 0}}/>
         );
     }
 }
