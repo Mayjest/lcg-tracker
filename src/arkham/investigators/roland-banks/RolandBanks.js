5b0546f32392e43b7cab37e3b91a2945d92ee47b
Mayjest <mayjest@gmail.com>
2021-06-03 15:41:48 +0100

    switching slowly to json data model

--- src/arkham/investigators/roland-banks/RolandBanks.js
+++ src/arkham/investigators/roland-banks/RolandBanks.js
@@ -1,24 +1,14 @@
 import React from 'react';
 import Investigator from '../Investigator';
 import rolandBanks from './RolandBanksProfile-64.png';
+import rolandData from './roland-data.json';
 
 class RolandBanks extends React.Component {
 
-    investigatorData = {
-        name: "Roland Banks",
-        class: "Guardian",
-        picture: rolandBanks,
-        maxDamage: 9,
-        maxSanity: 5,
-        stats: {
-            agility: 2,
-            willpower: 3,
-            intellect: 3,
-            combat: 4 
-        }
-    };
+    
 
     render() {
+        this.investigatorData = rolandData;
         return (
             <Investigator 
                 profile={this.investigatorData} status={{physicalTrauma: 3, mentalTrauma: 0}}/>
